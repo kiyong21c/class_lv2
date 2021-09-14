9b39d4bb0de02c00dc6fc4946974a7559ad3395c
KIYONG SEO <kiyong21c@naver.com>
2021-09-14 19:27:09 +0900

    2021.09.14(화)

--- p_chapter05_03.py
+++ p_chapter05_03.py
@@ -35,17 +35,17 @@ print(avg_closure1.__code__.co_freevars)    # 자유변수 확인해보자 → '
 print(avg_closure1.__closure__[0].cell_contents)    # [10, 30, 50]
 
 # 잘못된 클로저 사용 예
-def closure_ex2():
-    # Free variable
-    cnt = 0
-    total = 0
-    def averager(v):
-        cnt += 1    # UnboundLocalError: local variable 'cnt' referenced before assignment
-        total += v
-        return total / cnt
-    return averager
-avg_closure2 = closure_ex2()
-print(avg_closure2(10))
+# def closure_ex2():
+#     # Free variable
+#     cnt = 0
+#     total = 0
+#     def averager(v):
+#         cnt += 1    # UnboundLocalError: local variable 'cnt' referenced before assignment
+#         total += v
+#         return total / cnt
+#     return averager
+# avg_closure2 = closure_ex2()
+# print(avg_closure2(10))
 
 # 잘못된 클로저 사용 예 → 수정
 def closure_ex3():
